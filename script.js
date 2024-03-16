0bd6fc6aee1cc82bcf0e3298c7d487a7e22d60af
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-16 14:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your code here
-document.querySelector('form').addEventListener('submit', function(event) {
-  if (!event.target.checkValidity()) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
-  event.target.classList.add('was-validated');
-});
